449cd625fca06e5693219acaffd2147f33a72cc5
viktorvoltz <vctrchinyeaka@gmail.com>
2022-04-20 18:38:29 +0100

    main class

--- app/src/main/java/com/chinonso/wearos/MainActivity.java
+++ app/src/main/java/com/chinonso/wearos/MainActivity.java
@@ -1,22 +1,86 @@
 package com.chinonso.wearos;
 
 import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.view.View;
+import android.support.wearable.view.WatchViewStub;
+import android.util.Log;
+import android.view.WindowManager;
 import android.widget.TextView;
 
-import com.chinonso.wearos.databinding.ActivityMainBinding;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements SensorEventListener {
+
+    private static final String TAG = "MainActivity";
+    private TextView mTextViewStepCount;
+    private TextView mTextViewStepDetect;
+    private TextView mTextViewHeart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        // Keep the Wear screen always on (for testing only!)
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
+            @Override
+            public void onLayoutInflated(WatchViewStub stub) {
+                mTextViewStepCount = (TextView) stub.findViewById(R.id.step_count);
+                mTextViewStepDetect = (TextView) stub.findViewById(R.id.step_detect);
+                mTextViewHeart = (TextView) stub.findViewById(R.id.heart);
+                getStepCount();
+            }
+        });
     }
 
-    public void onclickbttn(View view){
-        TextView txtv = findViewById(R.id.text);
-        txtv.setText("hello chinonso");
+    private void getStepCount() {
+        SensorManager mSensorManager = ((SensorManager)getSystemService(SENSOR_SERVICE));
+        Sensor mHeartRateSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_HEART_RATE);
+        Sensor mStepCountSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+        Sensor mStepDetectSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR);
+
+        mSensorManager.registerListener(this, mHeartRateSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager.registerListener(this, mStepCountSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager.registerListener(this, mStepDetectSensor, SensorManager.SENSOR_DELAY_NORMAL);
     }
+
+    private String currentTimeStr() {
+        Calendar c = Calendar.getInstance();
+        SimpleDateFormat df = new SimpleDateFormat("HH:mm:ss");
+        return df.format(c.getTime());
+    }
+
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        Log.d(TAG, "onAccuracyChanged - accuracy: " + accuracy);
+    }
+
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_HEART_RATE) {
+            String msg = "" + (int)event.values[0];
+            mTextViewHeart.setText(msg);
+            Log.d(TAG, msg);
+        }
+        else if (event.sensor.getType() == Sensor.TYPE_STEP_COUNTER) {
+            String msg = "Count: " + (int)event.values[0];
+            mTextViewStepCount.setText(msg);
+            Log.d(TAG, msg);
+        }
+        else if (event.sensor.getType() == Sensor.TYPE_STEP_DETECTOR) {
+            String msg = "Detected at " + currentTimeStr();
+            mTextViewStepDetect.setText(msg);
+            Log.d(TAG, msg);
+        }
+        else
+            Log.d(TAG, "Unknown sensor type");
+    }
+
 }
